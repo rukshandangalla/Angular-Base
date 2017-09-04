312be2acf3c81ac8dc2df774cfebf1d626ab5993
Rukshan Dangalla <rukshandangalla@gmail.com>
2017-09-04 10:47:51 +0530

    code formatting

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -8,8 +8,10 @@ import {  } from "events";
 })
 export class StarComponent implements OnChanges {
     @Input() rating: number;
-    starWidth: number;
     @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+    
+    starWidth: number;
+    
     ngOnChanges(): void {
         this.starWidth = this.rating * 86 / 5;
     }
