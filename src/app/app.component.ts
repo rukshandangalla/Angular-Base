87f4a267cc35201e6123355d737c97980d5346e5
Rukshan Dangalla <rukshandangalla@gmail.com>
2017-09-04 15:14:36 +0530

    service adding completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'pm-root',
-  template: `
-      <div>
-        <h1>{{pageTitle}}</h1>
-        <pm-products></pm-products>
-      </div>`
+  template: `<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>`,
+  providers: [ProductService]
 })
 export class AppComponent {
   pageTitle: string = "Product Management";
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -7,13 +8,16 @@ import { IProduct } from './product';
     styleUrls: ['./product-list.components.css']
 })
 export class ProductListComponent implements OnInit {
-
     pageTitle: string = "Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImages: boolean = false;
 
+    filteredProducts: IProduct[];
+    products: IProduct[];
+
     _listFilter: string;
+    errorMessage: string;
 
     get listFilter(): string {
         return this._listFilter;
@@ -24,34 +28,8 @@ export class ProductListComponent implements OnInit {
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
 
-    filteredProducts: IProduct[];
+    constructor(private _productService: ProductService) {
 
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-    ];
-
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
     }
 
     performFilter(filterBy: string): IProduct[] {
@@ -66,7 +44,14 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        console.log('In On Init');
+        this._productService.getProducts()
+            .subscribe(
+            products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error => this.errorMessage = <any>error
+        );
     }
 
     onRatingClicked(message: string): void {
